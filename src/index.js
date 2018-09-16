3a51414c907f9b247c9e4faad2113fcfa9ed25be
SobolevYuri <yura.sobolev.91@gmail.com>
2018-09-16 14:44:30 +0300

    Final version (fixed edition)

--- src/index.js
+++ src/index.js
@@ -1,42 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  var ar=str;
 
-  var conf=bracketsConfig;
-  var stack=new Array(conf.length);
-  var stacknumb=new Array();
-for(let i=0;i<conf.length;i++){
-stack[i]=new Array();
-}
-  for (let i=0;i<str.length;i++)
-  {
-    for(let j=0;j<conf.length;j++)
-    {
-    if(ar[i]==conf[j][0])
-      { 
-        stack[j].push(ar[i]);
-        stacknumb.push(i);
-        break;
-      }
+  let stAr = str.split('');
+
+  for (let i = 0; i < stAr.length;i++){
+
+   	if(stAr[i+1] != undefined && stAr[i] != undefined){
+
+   		if ( (stAr[i] == '|' && stAr[i+1] == '|') || stAr[i] != stAr[i+1] || (stAr[i] == '8' && stAr[i+1] == '8') || (stAr[i] == '7' && stAr[i+1] == '7')){
+
+   		for (let j = 0; j < bracketsConfig.length;j++){
+   			for (let r = 0; r < bracketsConfig[j].length; r++){
+
+   				if(stAr[i] == bracketsConfig[j][r] && stAr[i+1] == bracketsConfig[j][r+1]){
+            stAr[i] = 0;
+            stAr[i+1] = 0;
+         
+   			for (let g = 0; g <= stAr.length; g++){
+         let a = stAr.indexOf(0);
+         
+   			if ( a >= 0){
+          stAr.splice(a,1);
+         }
+   			}
+   			i=-1;
+   		}
+   			}
+   		}
+   	}
+   	}
+   }
+
+   if (stAr.length == 0){
+   	return true;
+   }else{
+     return false;
     }
-    for(let j=0;j<conf.length;j++)
-    {
-      if(ar[i]==conf[j][1])
-      {
-        if(stack[j].length==0)return false;
-        if(ar[stacknumb.pop()]==conf[j][0])stack[j].pop();
-       
-        }
-        
-      
-    }
-      
-  }
-
-for(let j=0;j<conf.length;j++)
-{
-  if(stack[j].length>0)
-  {return false;}
 }
-return true;
-}
-
